34be31caf8c4e24eeb9086201bb22092cc957320
TsubasaSaito331 <saito.tsubasa.xk@tut.jp>
2024-10-14 07:45:11 +0900

    retry setting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -206,63 +206,88 @@ export async function registerGame(
   const resultsWithGamePoints = calcGamePoints(results.slice());
   console.log(resultsWithGamePoints);
 
+  const userId = cookies().get('user')?.value;
+
   // Insert data into the database
-  try {
-    const userId = cookies().get('user')?.value;
-    const newId = uuidv4();
-    // 試合結果の登録
-    await sql`
-      INSERT INTO games (Id,Date, EastPlayer, EastPlayerScore, SouthPlayer, SouthPlayerScore, WestPlayer, WestPlayerScore, NorthPlayer, NorthPlayerScore,UserId)
-      VALUES (
-        ${newId},
-        ${japanTimeString},
-        ${results[0].id},
-        ${results[0].score * 100},
-        ${results[1].id},
-        ${results[1].score * 100},
-        ${results[2].id},
-        ${results[2].score * 100},
-        ${results[3].id},
-        ${results[3].score * 100},
-        ${userId}
-        );
-    `;
-
-    // プレイヤーのスコアの更新
-    for (const player of resultsWithGamePoints) {
+  let success = false;
+  while (!success && retryCount < 3) {
+    try {
+      const newId = uuidv4();
+      // 試合結果の登録
       await sql`
-        UPDATE players
-        SET
-          TotalScore = TotalScore+${player.point},
-          RawScore = RawScore+${(player.score * 100 - 25000) / 1000},
-          Games = Games + 1,
-          FirstNum = FirstNum + ${player.rank == 1 ? 1 : 0},
-          SecondNum = SecondNum + ${player.rank == 2 ? 1 : 0},
-          ThirdNum = ThirdNum + ${player.rank == 3 ? 1 : 0},
-          FourthNum = FourthNum + ${player.rank == 4 ? 1 : 0},
-          MaxScore = CASE WHEN MaxScore < ${player.score * 100} THEN ${
-            player.score * 100
-          } ELSE MaxScore END
-        WHERE
-          Id =${player.id};
-        `;
-    }
-    return { message: 'Game registered successfully.' };
-  } catch (error) {
-    console.error('Database Error:', error);
+        INSERT INTO games (Id,Date, EastPlayer, EastPlayerScore, SouthPlayer, SouthPlayerScore, WestPlayer, WestPlayerScore, NorthPlayer, NorthPlayerScore,UserId)
+        VALUES (
+          ${newId},
+          ${japanTimeString},
+          ${results[0].id},
+          ${results[0].score * 100},
+          ${results[1].id},
+          ${results[1].score * 100},
+          ${results[2].id},
+          ${results[2].score * 100},
+          ${results[3].id},
+          ${results[3].score * 100},
+          ${userId}
+          );
+      `;
+      success = true;
+    } catch (error) {
+      console.error('Database Error:', error);
 
-    // リトライロジック (間隔200msで固定)
-    if (retryCount < 3) {
-      console.log(`Retrying registration in 100ms...`);
-      await new Promise((resolve) => setTimeout(resolve, 100));
-      return registerGame(results, date, retryCount + 1);
-    } else {
-      return {
-        message:
-          'Database Error: Failed to register game after multiple retries.',
-      };
+      // リトライロジック (間隔200msで固定)
+      if (retryCount < 3) {
+        console.log(`Retrying registration in 200ms...`);
+        await new Promise((resolve) => setTimeout(resolve, 200));
+        retryCount++;
+      } else {
+        return {
+          message:
+            'Database Error: Failed to register game after multiple retries.',
+        };
+      }
     }
   }
+
+  // プレイヤーのスコアの更新
+  for (const player of resultsWithGamePoints) {
+    success = false;
+    while (!success && retryCount < 3) {
+      try {
+        await sql`
+            UPDATE players
+            SET
+              TotalScore = TotalScore+${player.point},
+              RawScore = RawScore+${(player.score * 100 - 25000) / 1000},
+              Games = Games + 1,
+              FirstNum = FirstNum + ${player.rank == 1 ? 1 : 0},
+              SecondNum = SecondNum + ${player.rank == 2 ? 1 : 0},
+              ThirdNum = ThirdNum + ${player.rank == 3 ? 1 : 0},
+              FourthNum = FourthNum + ${player.rank == 4 ? 1 : 0},
+              MaxScore = CASE WHEN MaxScore < ${player.score * 100} THEN ${
+                player.score * 100
+              } ELSE MaxScore END
+            WHERE
+              Id =${player.id};
+            `;
+        success = true;
+      } catch (error) {
+        console.error('Database Error:', error);
+
+        // リトライロジック (間隔200msで固定)
+        if (retryCount < 3) {
+          console.log(`Retrying registration in 200ms...`);
+          await new Promise((resolve) => setTimeout(resolve, 200));
+          retryCount++;
+        } else {
+          return {
+            message:
+              'Database Error: Failed to register game after multiple retries.',
+          };
+        }
+      }
+    }
+  }
+  return { message: 'Game registered successfully.' };
 }
 
 export async function deleteGame(gameResult: GameResult, retryCount = 0) {
